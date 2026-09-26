f89888f2ca9f50f04088f8ac191b467b9a53729b
Joshua <joshuanivar16@gmail.com>
2026-09-25 22:36:44 -0400

    librerias y configuracion frontend instaladas

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,11 +1,14 @@
 import './assets/main.css'
-
+import { BrowserRouter } from 'react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
+
 )
